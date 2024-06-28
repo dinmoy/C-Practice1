d9c7ca1dfc0a60e1eb19671d4b1233b720298726
dinmoy <hyoin614@naver.com>
2024-06-28 09:44:58 +0900

    #1 12-6 증감연산자

--- C#Practice1/Program.cs
+++ C#Practice1/Program.cs
@@ -74,7 +74,12 @@ namespace C_Practice1
             output += "World";
             Console.WriteLine(output);
 
-       
+            //#1 12-6 증감연산자
+            int number = 10;
+            Console.WriteLine(number++); //10
+            Console.WriteLine(++number); //12
+            Console.WriteLine(number--); //12
+            Console.WriteLine(--number); //10
 
         }
     }
