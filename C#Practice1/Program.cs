57374b7eb4bcc298e4e6d3c494e7c1a22f18391f
dinmoy <hyoin614@naver.com>
2024-06-28 09:42:40 +0900

    #1 12-5 복합대입연산자

--- C#Practice1/Program.cs
+++ C#Practice1/Program.cs
@@ -67,8 +67,14 @@ namespace C_Practice1
             //#1 12-4 변수
             // 변수(Variable)
             string name = "문자열 자료형";
-            
 
+            //#1 12-5 복합대입연산자
+            // += -= * = /= %=
+            string output = "Hello";
+            output += "World";
+            Console.WriteLine(output);
+
+       
 
         }
     }
