1ab14e91eacb503c7ee01d18cad10a19ed3aacb6
dinmoy <hyoin614@naver.com>
2024-07-01 13:32:35 +0900

    #4 21-4. 인스턴스 변수

---
+++ C#Practice1/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Practice1
+{
+    internal class User
+    {
+        public string name;
+        public string phoneNumber;
+    }
+    class Product
+    {
+        public static double TAX_RATIO = 1.0;
+        public string name;
+        public int price;
+    }
+}
