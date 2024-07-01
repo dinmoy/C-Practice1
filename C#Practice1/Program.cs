3246030d65406f5e857de0e492b248d4c3cd08ea
dinmoy <hyoin614@naver.com>
2024-07-01 13:46:41 +0900

    #5 22-4. 메서드 오버로딩

--- C#Practice1/Program.cs
+++ C#Practice1/Program.cs
@@ -18,6 +18,11 @@ namespace C_Practice1
             //Console.WriteLine(instanceVar); 
             return (input >= 0) ? input : -input;
         }
+
+        //메서드 오버로딩할 때 주의할 점 --> 매개변수는 꼭 다르게!
+        //메서드 시그네이처(이름,매개변수)가 겹치면 안됨
+        //public static double Abs(int input) { return 0;  }
+        public static double Abs(double input) { return 0; }
     }
     class FirstClass
     {
@@ -206,6 +211,11 @@ namespace C_Practice1
 
             //#5 22-3. 클래스 메서드
 
+            //#5 22-4. 메서드 오버로딩
+            Console.WriteLine(MyMath.Abs(-10));    //Abs(int)
+            Console.WriteLine(MyMath.Abs(10.0));  //Abs(double)
+            Console.WriteLine(MyMath.Abs(-10L));  //Abs(double)
+            Console.WriteLine(MyMath.Abs(3.0f));  //Abs(double)
         }
     }
 }
